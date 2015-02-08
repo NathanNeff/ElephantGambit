0c068f846f03b969aeab1fdd3d52ad118dc7582d
Nathan Neff <nathan.neff@gmail.com>
2015-02-07 21:15:36 -0600

    Shell scripts to use lichess API

---
+++ data/get_many_games.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+URL="http://en.lichess.org/api/game?rated=1&nb=2&with_moves=1&with_opening=1"
+curl $URL
---
+++ data/get_one_game_pgn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl http://en.lichess.org/game/export/Qa7FJNk2.pgn
