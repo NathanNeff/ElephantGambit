117f93b986aa7db1faa31905be202f223d933f95
Nathan Neff <nathan.neff@gmail.com>
2015-02-07 21:16:18 -0600

    lichess game downloader improvements

--- data/lichess_game_downloader.py
+++ data/lichess_game_downloader.py
@@ -1,13 +1,27 @@
 #!/bin/env python
 import json
+import time
+import requests
+import sys
 
+max_games = 19
+sleep_time = 30
+max_time = 1 * 10
+max_list_requests = 1
+list_requests = 0
 imported_games = {}
+start_time = time.time()
+stop = False
 
 def get_game_request():
-    json_data = open('lichess.json')
-    data = json.load(json_data)
-    return data
+    url = "http://en.lichess.org/api/game?rated=1&nb=200"
+    req = requests.get(url)
+    # data = json.loads(req.json())
+    # list_requests = list_requests + 1
+    # json_data = open('lichess.json')
+    # data = json.load(json_data)
     # print json.dumps(data, indent=4)
+    return req.json()
 
 
 def get_game_info_from_request():
@@ -16,14 +30,35 @@ def get_game_info_from_request():
     for game in data["list"]:
         
         if game["id"] in imported_games:
-            print "Yep, game ", game["id"], " exists, and it's ", imported_games[game["id"]]
+            sys.stderr.write("Already imported game: %s\n" % game["id"])
             imported_games[game["id"]] = imported_games[game["id"]] + 1
         else:
-            print "Nope, game ", game["id"], " doesn't exist"
+            sys.stderr.write("New game found: %s\n" % game["id"])
+            download_game(game["id"])
             imported_games[game["id"]] = 1
 
-        print "----------------------------------------------------------------------"
-        print "game list has ", len(imported_games.keys()), "things in it"
+        sys.stderr.write("Game list has %i games\n" % len(imported_games.keys()))
 
+def download_game(game_id):
+    url = "http://en.lichess.org/%s/pgn" % game_id
 
-get_game_info_from_request()
+    sys.stderr.write("Downloading game:  %s\n" % url)
+    req = requests.get(url)
+
+    if req.status_code == 200:
+        sys.stderr.write("Request successful:  %s\n" % url)
+        print req.content + "\n\n"
+    else:
+        print req.content
+        stop = True
+
+def begin_import():
+    while keep_importing():
+        get_game_info_from_request()
+
+def keep_importing():
+    program_run_time = time.time() - start_time
+    games_imported = len(imported_games.keys())
+    return stop == False and program_run_time < max_time and games_imported < max_games and list_requests < max_list_requests
+
+begin_import()
