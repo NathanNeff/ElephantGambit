cc28f5bd9346785c8a50039c4483b86cee05929e
Nathan Neff <nathan.neff@gmail.com>
2015-02-08 16:27:50 -0600

    Fix local vars, use pgn_module
    to output PGN

--- data/lichess_game_downloader.py
+++ data/lichess_game_downloader.py
@@ -3,24 +3,28 @@ import json
 import time
 import requests
 import sys
+import pgn_module
 
-max_games = 19
-sleep_time = 30
-max_time = 1 * 10
-max_list_requests = 1
+max_games = 10000
+sleep_time = 60
+max_time = 600 * 30
+max_list_requests = 100
 list_requests = 0
 imported_games = {}
 start_time = time.time()
 stop = False
 
 def get_game_request():
-    url = "http://en.lichess.org/api/game?rated=1&nb=200"
+    # API docs:  https://github.com/ornicar/lila#http-api
+    url = "http://en.lichess.org/api/game?rated=1&nb=200&with_moves=1&with_opening=1"
     req = requests.get(url)
-    # data = json.loads(req.json())
-    # list_requests = list_requests + 1
+    global list_requests
+    list_requests = list_requests + 1
     # json_data = open('lichess.json')
     # data = json.load(json_data)
     # print json.dumps(data, indent=4)
+    # return req.json()
+    # sys.stderr.write("Game:  " + req.json())
     return req.json()
 
 
@@ -34,7 +38,8 @@ def get_game_info_from_request():
             imported_games[game["id"]] = imported_games[game["id"]] + 1
         else:
             sys.stderr.write("New game found: %s\n" % game["id"])
-            download_game(game["id"])
+            pgn_module.parse_lichess_json(game)
+            print pgn_module.pgnString(), "\n"
             imported_games[game["id"]] = 1
 
         sys.stderr.write("Game list has %i games\n" % len(imported_games.keys()))
@@ -55,9 +60,13 @@ def download_game(game_id):
 def begin_import():
     while keep_importing():
         get_game_info_from_request()
+        for i in range(sleep_time):
+            sys.stderr.write("Sleeping %d\n" % i)
+            time.sleep(1)
 
 def keep_importing():
     program_run_time = time.time() - start_time
+    global games_imported
     games_imported = len(imported_games.keys())
     return stop == False and program_run_time < max_time and games_imported < max_games and list_requests < max_list_requests
 
